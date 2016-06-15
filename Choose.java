c9a34772bb55ac246e1a81821069a57bd2ecd22b
LolaYu <lolayqzh@hotmail.com>
2016-06-15 02:49:38 -0400

    Update Choose.java

--- Choose.java
+++ Choose.java
@@ -2,8 +2,8 @@
 public class Choose {
 	
 	public static String[] visitor(){  
-			String[] list = { "Student", "Professor", "Business Person", "Blogger" };
-			return list;  
+	    String[] list = { "Student", "Professor", "Business Person", "Blogger" };
+	       return list;  
 	}
 	
 	public static String[] firstplace(){  
