0532244c7e40e2b71e0b503ee37b9f8d34d0161a
LolaYu <lolayqzh@hotmail.com>
2016-07-06 14:06:53 -0400

    Add files via upload

---
+++ Assignment2--CitySim9002/Choose.java
@@ -0,0 +1,21 @@
+
+public class Choose {
+	
+	public static String[] visitor(){  
+			String[] list = { "Student", "Professor", "Business Person", "Blogger" };
+			return list;  
+	}
+	
+	public static String[] firstplace(){  
+	    String[] list = { "The Cathedral of Learning", "Squirrel Hill", "The Point", "Downtown" };
+		return list;  		
+	    }  
+	
+	
+	public static String[] otherplace(){  
+	    String[] list = { "The Cathedral of Learning", "Squirrel Hill", "The Point", "Downtown","leave" };
+		return list;  		
+	    }  
+	
+
+}
